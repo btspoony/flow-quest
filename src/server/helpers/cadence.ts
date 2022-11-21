a38f92a9b4bfa91ff9a5590f0b42cb79f7d6d64f
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-11-21 15:14:10 +0800

    chore: upgrade version

--- src/server/helpers/cadence.ts
+++ src/server/helpers/cadence.ts
@@ -1,16 +1,16 @@
 /**
  * transactions for dev challenges
  */
-export { default as txProfileRegister } from "../../../cadence/dev-challenge/transactions/profile-register.cdc";
-export { default as txAdminAddQuestConfig } from "../../../cadence/dev-challenge/transactions/admin-add-quest-cfg.cdc";
-export { default as txAdminStartNewSeason } from "../../../cadence/dev-challenge/transactions/admin-start-new-season.cdc";
-export { default as txAdminUpdateEndDate } from "../../../cadence/dev-challenge/transactions/admin-update-end-date.cdc";
-export { default as txCtrlerAppendQuestParams } from "../../../cadence/dev-challenge/transactions/ctrler-append-quest-params.cdc";
-export { default as txCtrlerSetQuestCompleted } from "../../../cadence/dev-challenge/transactions/ctrler-set-quest-completed.cdc";
-export { default as txCtrlerSetQuestFailure } from "../../../cadence/dev-challenge/transactions/ctrler-set-quest-failure.cdc";
-export { default as txCtrlerSetupReferralCode } from "../../../cadence/dev-challenge/transactions/ctrler-setup-referral-code.cdc";
+export { default as txProfileRegister } from "../../../cadence/dev-challenge/transactions/profile-register.cdc?raw";
+export { default as txAdminAddQuestConfig } from "../../../cadence/dev-challenge/transactions/admin-add-quest-cfg.cdc?raw";
+export { default as txAdminStartNewSeason } from "../../../cadence/dev-challenge/transactions/admin-start-new-season.cdc?raw";
+export { default as txAdminUpdateEndDate } from "../../../cadence/dev-challenge/transactions/admin-update-end-date.cdc?raw";
+export { default as txCtrlerAppendQuestParams } from "../../../cadence/dev-challenge/transactions/ctrler-append-quest-params.cdc?raw";
+export { default as txCtrlerSetQuestCompleted } from "../../../cadence/dev-challenge/transactions/ctrler-set-quest-completed.cdc?raw";
+export { default as txCtrlerSetQuestFailure } from "../../../cadence/dev-challenge/transactions/ctrler-set-quest-failure.cdc?raw";
+export { default as txCtrlerSetupReferralCode } from "../../../cadence/dev-challenge/transactions/ctrler-setup-referral-code.cdc?raw";
 
 /**
  * scripts for quest checking
  */
-export { default as scVerifyQuestS1Q1 } from "../../../cadence/quests/S1Q1/verify-script.cdc";
+export { default as scVerifyQuestS1Q1 } from "../../../cadence/quests/S1Q1/verify-script.cdc?raw";
