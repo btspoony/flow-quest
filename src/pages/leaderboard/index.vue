0795ad3b0b8a57bdde1de34bf78e2e490c03f795
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-12-21 22:07:04 +0800

    feat: fix no wallet

--- src/pages/leaderboard/index.vue
+++ src/pages/leaderboard/index.vue
@@ -7,28 +7,25 @@ watch(wallet, (newVal, oldVal) => {
 
 const { data: info, pending, refresh } = useAsyncData<RankingStatus | null>(`ranking`, async () => {
   const { $scripts } = useNuxtApp();
-  if (wallet.value) {
-    const result = await $scripts.getRankingStatus(100, wallet.value?.addr)
-    let addrs: string[] = []
-    if (result?.tops.length && result?.tops.length > 0) {
-      addrs = result.tops.map(one => one.account)
-    }
-    if (result?.account) {
-      addrs.push(result?.account.account)
-    }
-    if (addrs.length > 0) {
-      const identities = await $scripts.profilesGetIdentity(addrs)
-      result?.tops.forEach(one => {
-        one.identity = identities.find(id => id.account === one.account)?.identity
-      })
-      if (result?.account) {
-        result.account.identity = identities.find(id => id.account === result?.account?.account)?.identity
-      }
-    }
-    return result
-  } else {
-    return null
+  let address = wallet.value && wallet.value?.addr
+  const result = await $scripts.getRankingStatus(100, address)
+  let addrs: string[] = []
+  if (result?.tops.length && result?.tops.length > 0) {
+    addrs = result.tops.map(one => one.account)
   }
+  if (result?.account) {
+    addrs.push(result?.account.account)
+  }
+  if (addrs.length > 0) {
+    const identities = await $scripts.profilesGetIdentity(addrs)
+    result?.tops.forEach(one => {
+      one.identity = identities.find(id => id.account === one.account)?.identity
+    })
+    if (result?.account) {
+      result.account.identity = identities.find(id => id.account === result?.account?.account)?.identity
+    }
+  }
+  return result
 }, {
   server: false
 });
@@ -44,7 +41,7 @@ const { data: info, pending, refresh } = useAsyncData<RankingStatus | null>(`ran
     </template>
   </section>
   <section class="mb-0">
-    <h5>Top users in the season</h5>
+    <h5>Top 100 of current season</h5>
     <div v-if="pending" class="w-full h-20" :aria-busy="true" />
     <template v-else>
       <ItemLeaderboardBar v-for="(one, index) in info?.tops" :key="`item_${index}`" :score="one" />
