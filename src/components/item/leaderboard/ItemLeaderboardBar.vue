009b25fbe9307721cdc0918d57236a53a8bcf64f
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-12-21 18:36:17 +0800

    feat: leaderboard bar

--- src/components/item/leaderboard/ItemLeaderboardBar.vue
+++ src/components/item/leaderboard/ItemLeaderboardBar.vue
@@ -1,12 +1,44 @@
 <script setup lang="ts">
-const props = defineProps<{
+defineProps<{
   score: RankingScore
 }>()
 </script>
 
 <template>
-  <div class="card card-border p-4 w-full flex gap-4 items-center justify-between">
-    <div class="flex-none">11</div>
-    <div class="">{{ JSON.stringify(score) }}</div>
+  <div class="card card-border p-4 w-full flex gap-4 items-center justify-around">
+    <div class="flex-none">
+      <span v-if="score.rank === 0" class="circle-badge border-[#FFF71D] bg-yellow-400 text-white">
+        {{ score.rank + 1 }}
+      </span>
+      <span v-else-if="score.rank === 1" class="circle-badge border-gray-100 bg-gray-400 text-white">
+        {{ score.rank + 1 }}
+      </span>
+      <span v-else-if="score.rank === 2" class="circle-badge border-orange-200 bg-amber-600 text-white">
+        {{ score.rank + 1 }}
+      </span>
+      <span v-else class="circle-badge !border-none bg-gray-200 dark:bg-gray-800">
+        {{ score.rank + 1 }}
+      </span>
+    </div>
+    <div class="flex-none inline-flex rounded-[50%] overflow-hidden">
+      <img v-if="score.identity?.display.thumbnail" class="max-w-[2.5rem] max-h-[2.5rem]"
+        :src="score.identity.display.thumbnail" :alt="`PFP of ${score.identity.display.name}`" />
+    </div>
+    <div class="flex-auto">
+      <div class="flex flex-col items-start">
+        <span class="font-semibold">{{ score.identity?.display.name }}</span>
+        <span class="text-xs">{{ getShortAddress(score.account) }} </span>
+      </div>
+    </div>
+    <div class="flex-none">
+      <span class="font-bold text-black dark:text-white">{{ score.score }}</span> Points
+    </div>
   </div>
 </template>
+
+<style scoped>
+.circle-badge {
+  @apply rounded-full inline-block w-8 h-8 flex-center;
+  @apply border-solid border-2;
+}
+</style>
