d0bf27bd14dd2e6dca3d7a24ca1e36ed4113b43d
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-12-29 16:49:35 +0800

    feat: update year

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import GithubIcon from '~/assets/svgs/github.svg'
-
 </script>
 
 <template>
   <footer class="w-full h-12 mt-4">
     <div class="h-full container flex-between">
-      <small>Copyright © 2022 Flow</small>
+      <small>Copyright © {{ new Date().getFullYear() }} Flow</small>
       <div class="flex-center gap-2">
         <NuxtLink to="https://github.com/btspoony/flow-challenge-tour" target="_blank" class="secondary">
           <GithubIcon class="fill-current w-6 h-6" />
