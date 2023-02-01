9a7c07998a78cc9615ac1dc361c336cbea5973dc
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2023-02-01 19:57:59 +0800

    feat: update settings

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -4,15 +4,46 @@ const { data, pending } = useAsyncData<CompetitionSeason | null>('season', async
 }, {
   server: false
 });
+
+const route = useRoute()
+
+const currentTab = ref(route.query.tab === 'bounties' ? 'bounties' : 'seasons')
+const tabs = [
+  { label: "seasons" },
+  { label: "bounties" },
+]
+
+watch(() => route.query, (newQuery) => {
+  if (newQuery.tab === 'bounties') {
+    currentTab.value = 'bounties'
+  } else {
+    currentTab.value = 'seasons'
+  }
+})
+
+function updateRoute(name: string) {
+  const router = useRouter()
+  router.replace({ path: route.path, query: { tab: name } })
+}
 </script>
 
 <template>
   <FrameAdmin>
     <WidgetLoadingCard v-if="pending" />
-    <div v-else class="w-full flex flex-col items-start gap-4 pt-8">
-      <SectionSettingsBounties />
-      <SectionSettingsCurrent v-if="data?.seasonId" />
-      <SectionSettingsNew v-else />
-    </div>
+    <template v-else>
+      <nav class="py-4">
+        <ul class="tabs">
+          <li v-for="tab in tabs" :key="tab.label" :class="['tab-link', { 'active': currentTab === tab.label }]"
+            @click="updateRoute(tab.label)">
+            {{ tab.label }}
+          </li>
+        </ul>
+      </nav>
+      <SectionSettingsBounties v-if="currentTab === 'bounties'" />
+      <div v-else-if="currentTab === 'seasons'" class="w-full flex items-start gap-4">
+        <SectionSettingsCurrent v-if="data?.seasonId" />
+        <SectionSettingsNew v-else />
+      </div>
+    </template>
   </FrameAdmin>
 </template>
