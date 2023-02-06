a8147e1a6d0346ace34001387ec79ca30993f9f0
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2023-02-06 21:10:39 +0800

    feat: update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,11 +5,14 @@ import { Icon } from '@iconify/vue';
 <template>
   <footer class="w-full h-12 mt-4">
     <div class="h-full container flex-between">
-      <small>
-        Copyright © {{ new Date().getFullYear() }} Flow | Get support on
-        <NuxtLink to="https://discord.gg/Ekw2T9Zenf" target="_blank">Emerald City DAO</NuxtLink>
-      </small>
+      <small>© {{ new Date().getFullYear() }} All rights reserved.</small>
       <div class="flex-center gap-2">
+        <small>Get support on
+          <NuxtLink to="https://discord.gg/emeraldcity" target="_blank">Emerald City DAO</NuxtLink>
+        </small>
+        <NuxtLink to="https://twitter.com/btspoony" target="_blank" rel="noopener noreferrer">
+          <Icon icon="logos:twitter" class="w-5 h-5" />
+        </NuxtLink>
         <NuxtLink to="https://github.com/btspoony/flow-challenge-tour" target="_blank" class="secondary">
           <Icon icon="uil:github" class="w-6 h-6" />
         </NuxtLink>
