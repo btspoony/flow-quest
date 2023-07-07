401d4cd3788430be05fcecd84e29ecaf5de9e26c
Tang Bo Hao <tech@btang.cn>
2023-07-07 19:55:13 +0800

    fix: fix subscription error by hacking

--- src/server/helpers/flow.mjs
+++ src/server/helpers/flow.mjs
@@ -1,5 +1,17 @@
 import * as fcl from "@onflow/fcl";
 
+// FCL_REGISTRY Hacking
+const root =
+  (typeof self === "object" && self.self === self && self) ||
+  (typeof global === "object" && global.global === global && global) ||
+  (typeof window === "object" && window.window === window && window);
+
+root.FCL_REGISTRY = root.FCL_REGISTRY == null ? {} : root.FCL_REGISTRY;
+
+if (root.FCL_REGISTRY?.config !== undefined) {
+  root.FCL_REGISTRY.config?.subs?.clear()
+}
+
 export const APP_IDENTIFIER = "Flow Dev Quest V1.0";
 
 export function switchToMainnet() {
